6b6f8b6475abe624093102c3eccf54d30ad51fcf
John Goerzen <jgoerzen@complete.org>
2008-04-24 05:39:14 -0500

    Revert "tarenc-scanner initial conversion done"
    This reverts commit 87f1b997eec08e31ca08762c3320d828fe7d7827.
    
    Conflicts:
    
    	tarenc-encoder.hs

--- tarenc-scanner.hs
+++ tarenc-scanner.hs
@@ -24,35 +24,21 @@ import System.Environment
 import HSH
 import System.IO
 import Control.Monad
-import System.Path(brackettmpdir)
-import System.Posix.Files(createNamedPipe)
 
 main :: IO ()
-main = brackettmpdir "tarenc-scanner.XXXXXX" $ \tmpdir ->
+main = 
     do --updateGlobalLogger "" (setLevel INFO)
        argv <- getArgs
-       blocksfn <- case argv of
+       datafn <- case argv of
                    [x] -> return x
                    _ -> usage
 
-       {- We use tee to send our raw tar data to stdout and tarfifo.
-          Then we can fire up a second process that uses tar to copy
-          from that fifo to blocksfn. -}
+       runIO $ teeFIFOBS [datafn] -|- ("tar", ["-Rtf", "-"])
 
-       let tarfifofn = tmpdir ++ "/scannerfifo"
-       createNamedPipe tarfifofn 0o600
-
-       cmd1r <- run $ teeFIFOBS [tarfifofn]
-       cmd2r <- run $ ("cat", [tarfifofn]) -|- 
-                ("tar", ["-Rtf", "-"]) -|- catToFIFOBS blocksfn
-
-       cmd1r >>= checkResults
-       cmd2r >>= checkResults
-       
 usage :: IO a           
 usage =
     do putStrLn "Usage:\n"
        putStrLn "tarenc-scanner tarfifoname"
-       putStrLn "input from stdin, output blocks to tarfifoname, and input copied"
-       putStrLn "to stdout."
+       putStrLn "input from stdin, output blocks to stdout, and input copied"
+       putStrLn "to given fifoname."
        fail "Usage error"
